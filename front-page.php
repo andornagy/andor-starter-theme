62e6fc42d2e4386fa45e556e283cb3af8821e78e
andornagym <andornagy@outlook.com>
2022-08-14 21:13:18 +0300

    Local test

--- front-page.php
+++ front-page.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 <main class="section grid-container">
-    <div class="grid-x grid-margin-x grid-padding-y">
-        <div class="cell content">
-            <?php
-            echo '<pre>';
-            print_r(getSubMenu(7));
-            echo '</pre>";'
-            ?>
-            <?php the_content(); ?>
-        </div>
-    </div>
+   <div class="grid-x grid-margin-x grid-padding-y">
+      <div class="cell content">
+         <?php
+         echo '<pre>';
+         print_r(getSubMenu(7));
+         echo '</pre>';
+         ?>
+         <?php the_content(); ?>
+      </div>
+   </div>
 </main>
 <?php get_footer(); ?>
