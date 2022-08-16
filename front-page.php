d9b2c0fc07df59c55d1f20c8c4109a25b704c727
gusevdigital <gusevdigital@gmail.com>
2022-08-16 15:03:09 +0200

    Add deployignore

--- front-page.php
+++ front-page.php
@@ -2,11 +2,6 @@
 <main class="section grid-container">
     <div class="grid-x grid-margin-x grid-padding-y">
         <div class="cell content">
-            <?php
-            echo '<pre>';
-            print_r(getSubMenu(7));
-            echo '</pre>";'
-            ?>
             <?php the_content(); ?>
         </div>
     </div>
