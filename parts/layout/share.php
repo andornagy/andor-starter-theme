937f3576cd8709af802466c8266b4ddc9c28e35e
andornagym <andornagy@outlook.com>
2023-02-08 15:52:00 +0200

    Sharebuttons

--- parts/layout/share.php
+++ parts/layout/share.php
@@ -1,31 +1,16 @@
 <?php
 
 $id = get_the_id();
-$sitename = get_bloginfo('name');
-$twitter = get_field('twitter', 'option');
+$hideshare = get_field('hide_share_buttons');
+$postUrl = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 
-$url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-
-if (function_exists('YoastSEO')) {
-   $title = YoastSEO()->meta->for_post($id)->title;
-} else {
-   $title = get_the_title() . ' | ' . $sitename;
-}
-
-$title = urlencode($title);
-$sitename = urlencode($sitename);
-
-$share_disabled = get_field('disable_share');
-if (!$share_disabled) {
-
-?>
-
+if (!$hideshare == 'Yes') { ?>
 
    <section class="share-wrapper">
       <h2>Share this</h2>
       <ul class="menu share">
-         <li><a target="_blank" class="share-button share-twitter" href="https://twitter.com/intent/tweet?url=<?php echo $postUrl; ?>&text=<?php echo the_title(); ?>&via=<?php echo $twitter; ?>" title="Tweet this"><i class="fa-brands fa-twitter-square"></i></a></li>
-         <li><a target="_blank" class="share-button share-linkedin" href="https://linkedin.com/shareArticle?mini=true&url=<?php echo $url; ?>&title=<?php echo $title; ?>&source=<?php echo $sitename; ?>" title="Share on LinkedIn"><i class="fa-brands fa-linkedin"></i></a></li>
+         <li><a target="_blank" class="share-button share-twitter" href="https://twitter.com/intent/tweet?url=<?php echo $postUrl; ?>&text=<?php echo the_title(); ?>&via=<?php the_author_meta('twitter'); ?>" title="Tweet this"><?php echo getIcon('twitter'); ?></a></li>
+         <li><a target="_blank" class="share-button share-linkedin" href="https://linkedin.com/shareArticle?mini=true&url=<?php echo $postUrl; ?>&title=<?php echo $title; ?>&source=<?php echo $sitename; ?>" title="Share on LinkedIn"><?php echo getIcon('linkedin'); ?></a></li>
       </ul>
    </section>
 
