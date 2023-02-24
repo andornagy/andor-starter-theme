3c22170889496798c6cfe38229080af90848865b
andornagym <andornagy@outlook.com>
2023-02-24 15:17:46 +0200

    add meta data to loop list

--- parts/loop/loop-list.php
+++ parts/loop/loop-list.php
@@ -1,6 +1,16 @@
 <?php
 $id = isset($args['id']) && $args['id'] ? $args['id'] : get_the_ID();
 
+$postType = get_post_type($id);
+
+$articles = ['post', 'publication', 'newsletter'];
+
+$meta = null;
+if (in_array($postType, $articles)) {
+   $meta = get_the_time(get_option('date_format'), $id);
+} elseif ($postType === 'event') {
+   $meta = get_field('start_date') ? get_field('start_date') : '';
+}
 
 ?>
 <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" id="post-<?php the_ID(); ?>" class="cell posts-item__list">
@@ -8,5 +18,6 @@ $id = isset($args['id']) && $args['id'] ? $args['id'] : get_the_ID();
       <h3 class="posts-item__title">
          <?php the_title(); ?>
       </h3>
+      <?php echo $meta ? '<span class="posts-item__meta">' .  $meta . '</span>' : ''; ?>
    </div>
 </a>
