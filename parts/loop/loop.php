a279f1e1375a05ba699f2aeb8663ce3fb4e28d3e
andornagym <andornagy@outlook.com>
2023-03-03 12:53:29 +0200

    post slides template part

--- parts/loop/loop.php
+++ parts/loop/loop.php
@@ -1,20 +1,26 @@
 <?php
 $id = isset($args['id']) && $args['id'] ? $args['id'] : get_the_ID();
-$columns = 'cell large-4 medium-6';
-if (isset($args['columns'])) {
-   $columns = getColumns($args['columns'], $columns); // see helpers.php
+
+// columns
+if (isset($args['no_columns']) && $args['no_columns']) $columns = '';
+else {
+   $columns = 'cell large-4 medium-6';
+   if (isset($args['columns'])) {
+      $columns = getColumns($args['columns'], $columns); // see helpers.php
+   }
 }
+
 $thumb_url = sqeGetThumbnailURL($id, 'landscape');
 ?>
-<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" id="post-<?php the_ID(); ?>" <?php post_class('posts-item ' . esc_attr($columns)); ?>>
+<a href="<?php echo get_the_permalink($id); ?>" title="<?php echo get_the_title($id); ?>" <?php post_class('posts-item ' . esc_attr($columns)); ?>>
    <div class="posts-item__img rectangle-img">
-      <img src="<?php echo $thumb_url; ?>" alt="<?php echo $name; ?>" />
+      <img src="<?php echo $thumb_url; ?>" alt="<?php echo get_the_title($id); ?>" />
    </div>
    <div class="posts-item__content">
       <h3 class="posts-item__title">
-         <?php the_title(); ?>
+         <?php echo get_the_title($id); ?>
       </h3>
       <div class="posts-item__meta margin-bottom-1"><?php echo get_the_time(get_option('date_format'), $id); ?></div>
-      <?php echo has_excerpt() ? wpautop(get_the_excerpt()) : wpautop(wp_trim_words(get_the_content(), 24, '...')); ?>
+      <?php echo has_excerpt($id) ? wpautop(get_the_excerpt($id)) : wpautop(wp_trim_words(get_the_content($id), 24, '...')); ?>
    </div>
 </a>
