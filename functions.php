9558fec0e967ff1475d9201978a4dde2941f9b67
Trevor S <7655031+TrevorSqE@users.noreply.github.com>
2023-04-03 12:58:20 +0200

    Rename enqueue.php.

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ define('WEBSITE_SLUG', 'theme-name');
 
 require_once(get_theme_file_path('/functions/_init.php'));
 require_once(get_theme_file_path('/functions/foundation.php'));
-require_once(get_theme_file_path('/functions/enqueue-scripts.php'));
+require_once(get_theme_file_path('/functions/enqueue.php'));
 require_once(get_theme_file_path('/functions/helpers.php'));
 require_once(get_theme_file_path('/functions/acf.php'));
 require_once(get_theme_file_path('/functions/login.php'));
