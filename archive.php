f7ffe22c976c5e09a16785925082477969e4eda7
andornagym <andornagy@outlook.com>
2024-03-16 10:13:56 +0200

    first commit

--- archive.php
+++ archive.php
@@ -3,40 +3,38 @@
 <?php get_template_part('parts/banners/banner'); ?>
 
 <main class="section grid-container">
-	
-	<?php get_template_part('parts/layout/breadcrumbs'); ?>
-        
-    <section class="grid-x grid-padding-x grid-padding-y main">
-	    
-	    <div class="cell small-12 medium-2 large-3">
-	        <?php get_sidebar('left'); ?>
-        </div>
-	    
-        <div class="cell small-12 medium-8 large-6 content">
-	        
-		<?php
-			
-        if (have_posts()) {
-            while (have_posts()) {
-                the_post();
-                get_template_part('parts/loop/loop', get_post_type());
-            }
-        } else {
-            echo '<div class="cell"><h2>' . __('No posts found.', 'squareeye') . '</h2></div>';
+
+  <?php get_template_part('parts/layout/breadcrumbs'); ?>
+
+  <section class="grid-x grid-padding-x grid-padding-y main">
+
+    <div class="cell small-12 medium-2 large-3">
+      <?php get_sidebar('left'); ?>
+    </div>
+
+    <div class="cell small-12 medium-8 large-6 content">
+
+      <?php
+
+      if (have_posts()) {
+        while (have_posts()) {
+          the_post();
+          get_template_part('parts/loop/loop', get_post_type());
         }
-        echo paginate_links();
-        ?>
-            
-        </div>
-        
-        <div class="cell small-12 medium-2 large-3">
-	        <?php get_sidebar('right'); ?>
-        </div>
-        
-    </section>   
-    
+      } else {
+        echo '<div class="cell"><h2>' . __('No posts found.', 'squareeye') . '</h2></div>';
+      }
+      echo paginate_links();
+      ?>
+
+    </div>
+
+    <div class="cell small-12 medium-2 large-3">
+      <?php get_sidebar('right'); ?>
+    </div>
+
+  </section>
+
 </main>
 
 <?php get_footer(); ?>
-
-
